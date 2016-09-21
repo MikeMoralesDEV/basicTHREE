903ca8f21afc64bb51ebe7654b2fa93ef5c3ca67
Miguel Morales <mikemoralesdev@gmail.com>
2016-09-21 21:03:20 +0200

    Update basicThree.js

--- docs/js/basicThree.js
+++ docs/js/basicThree.js
@@ -342,7 +342,7 @@ basicTHREE.avatarLive = function(MovingMesh, cond=1, glob=0, rotate=0, reset=1){
 	if(basicTHREE.resetPos==undefined){
 		basicTHREE.Avatar = MovingMesh; 
 		basicTHREE.Avatar.add(basicTHREE.camera);
-		basicTHREE.camera.y = 90 * Math.PI / 180
+		basicTHREE.position.y = 200;
 		basicTHREE.resetPos=[MovingMesh.position.x, MovingMesh.position.y, MovingMesh.position.z]; //estos no varian para el reset.
 		basicTHREE.resetRot=[MovingMesh.rotation.x, MovingMesh.rotation.y, MovingMesh.rotation.z];
 	}
