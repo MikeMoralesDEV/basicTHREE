b766e80383a2de721ecf14b7470dad36341f98f6
Miguel Morales <mikemoralesdev@gmail.com>
2016-09-21 20:19:53 +0200

    Update basicThree.js

--- docs/js/basicThree.js
+++ docs/js/basicThree.js
@@ -331,7 +331,7 @@ basicTHREE.createCameraControls = function(){
 
 	var controls = new THREE.OrbitControls(basicTHREE.camera, basicTHREE.renderer.domElement);
 	controls.enableKeys = false; 
-
+	controls.rotation.y = 90 * Math.PI / 180;
 	return controls;
 
 };
